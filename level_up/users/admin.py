8f16d8ea7c029df9b231570ba2f6b03231f2b5da
sound-round <yudaev1@gmail.com>
2022-05-15 17:46:00 +0300

    Start project

---
+++ level_up/users/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from task_manager.user.models import User
+
+
+admin.site.register(User)
---
+++ level_up/users/serializers.py
@@ -0,0 +1,13 @@
+import imp
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = (
+			'username',
+			'email',
+			'password',
+		)
